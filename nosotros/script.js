eaee57429e5cad6e81cc3d1ac1135ffb1e2714ec
Sumejia99 <sarahirivassph@gmail.com>
2023-04-28 00:58:37 -0600

    Creacion de la pagina de nosotros

---
+++ nosotros/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-carousel', {
+      type: 'loop', // Habilita el modo de bucle para el carrusel
+      perMove: 1, // Configura la cantidad de diapositivas que se mueven con cada cambio (en este caso, 1)
+      autoplay: true, // Habilita el autoplay
+      interval: 5000, // Configura el tiempo de reproducción automática en milisegundos (en este caso, 5000 ms o 5 segundos)
+    }).mount();
+  });
+
+// Scroll-Up
+
+const irArriba = document.querySelector(".ir-arriba");
+
+irArriba.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY < 500) {
+        irArriba.style.right = -100 + "px";
+    } else {
+        irArriba.style.right = 5 + "px";
+    }
+});
