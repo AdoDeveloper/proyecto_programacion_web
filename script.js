484eebbc0e6e69ee20c24b6246975d9655a512bd
AdoDeveloper <ayudacuenta541@gmail.com>
2023-04-27 23:29:20 -0600

    se modificó el script.js, se agregó la funcionalidad del botón de ir arriba

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ document.addEventListener('DOMContentLoaded', function () {
       interval: 5000, // Configura el tiempo de reproducción automática en milisegundos (en este caso, 5000 ms o 5 segundos)
     }).mount();
   });
+
+  // Scroll-Up
+
+const irArriba = document.querySelector(".ir-arriba");
+
+irArriba.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY < 600) {
+        irArriba.style.right = -100 + "px";
+    } else {
+        irArriba.style.right = 5 + "px";
+    }
+});
